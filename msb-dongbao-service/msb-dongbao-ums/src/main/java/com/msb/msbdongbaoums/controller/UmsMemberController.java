b3662e6568cecfdfa358ca07fe256ceceb58166c
yunpengli <liyunpeng608@gmail.com>
2021-07-09 12:37:09 +0800

    first commit

---
+++ msb-dongbao-service/msb-dongbao-ums/src/main/java/com/msb/msbdongbaoums/controller/UmsMemberController.java
@@ -0,0 +1,21 @@
+package com.msb.msbdongbaoums.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 后台用户表 前端控制器
+ * </p>
+ *
+ * @author yunpengli
+ * @since 2021-07-09
+ */
+@RestController
+@RequestMapping("/ums-member")
+public class UmsMemberController {
+
+}
+
---
+++ msb-dongbao-service/msb-dongbao-ums/src/main/java/com/msb/msbdongbaoums/service/UmsMemberService.java
@@ -0,0 +1,16 @@
+package com.msb.msbdongbaoums.service;
+
+import com.msb.msbdongbaoums.entity.UmsMember;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 后台用户表 服务类
+ * </p>
+ *
+ * @author yunpengli
+ * @since 2021-07-09
+ */
+public interface UmsMemberService extends IService<UmsMember> {
+
+}
